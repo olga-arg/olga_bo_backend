796a25624dbdcacc192c74a53d235d4198be1d03
Valentin Vila <vilavalentin@gmail.com>
2023-03-04 23:53:52 +0100

    create go-lambda-create-user lambda

--- lambdas/go-lambda-create-user/pkg/handler/handler.go
+++ lambdas/go-lambda-create-user/pkg/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"go-lambda-create-user/internal/application"
 	"go-lambda-create-user/pkg/dto"
 	"net/http"
@@ -11,15 +12,22 @@ import (
 func CreateUser(app *application.App) func(ctx context.Context, request *http.Request) (response interface{}, err error) {
 	return func(ctx context.Context, request *http.Request) (response interface{}, err error) {
 		var input dto.UserInput
+		fmt.Println("input: ", input)
 		if err := json.NewDecoder(request.Body).Decode(&input); err != nil {
+			fmt.Println("error 1: ")
 			return nil, err
 		}
 		userOutput, err := app.Processor.CreateUser(ctx, &input)
+		fmt.Println("userOutput: ", userOutput)
 		if err != nil {
+			fmt.Println("error 2: ")
 			return nil, err
 		}
+		fmt.Println("going to be marshalled: ")
 		response, err = json.Marshal(userOutput)
+		fmt.Println("response: ", response)
 		if err != nil {
+			fmt.Println("error 3: ")
 			return nil, err
 		}
 		return response, nil
