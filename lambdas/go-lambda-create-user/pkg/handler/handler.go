9284f89d8f4f7c647e796c8c717cf264db6b99f8
Valentin Vila <vilavalentin@gmail.com>
2023-03-05 00:41:36 +0100

    create go-lambda-create-user lambda

--- lambdas/go-lambda-create-user/pkg/handler/handler.go
+++ lambdas/go-lambda-create-user/pkg/handler/handler.go
@@ -2,15 +2,38 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"go-lambda-create-user/internal/application"
+	"go-lambda-create-user/pkg/dto"
 	"net/http"
 )
 
 func CreateUser(app *application.App) func(ctx context.Context, request *http.Request) (response interface{}, err error) {
-	fmt.Println("TESTING!!!")
+	fmt.Println("TESTING2")
 	return func(ctx context.Context, request *http.Request) (response interface{}, err error) {
-		fmt.Println("TESTING2!!!")
-		return response, err
+		var input dto.UserInput
+		fmt.Println("input: ", input)
+		if err := json.NewDecoder(request.Body).Decode(&input); err != nil {
+			fmt.Println("error 1: ")
+			return nil, err
+		}
+		fmt.Println("BUENAS")
+		return processCreateUserRequest(ctx, app, &input)
 	}
+}
+
+func processCreateUserRequest(ctx context.Context, app *application.App, input *dto.UserInput) (response interface{}, err error) {
+	fmt.Println("BUENAS2")
+	userOutput, err := app.Processor.CreateUser(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err = json.Marshal(userOutput)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
 }
