3a0c30e2c3e88cd60fe528b94869ac9764f7241e
Valentin Vila <vilavalentin@gmail.com>
2023-03-05 00:14:19 +0100

    create go-lambda-create-user lambda

--- lambdas/go-lambda-create-user/pkg/handler/handler.go
+++ lambdas/go-lambda-create-user/pkg/handler/handler.go
@@ -3,31 +3,31 @@ package handler
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"go-lambda-create-user/internal/application"
 	"go-lambda-create-user/pkg/dto"
+	"log"
 	"net/http"
 )
 
 func CreateUser(app *application.App) func(ctx context.Context, request *http.Request) (response interface{}, err error) {
 	return func(ctx context.Context, request *http.Request) (response interface{}, err error) {
 		var input dto.UserInput
-		fmt.Println("input: ", input)
+		log.Println("input: ", input)
 		if err := json.NewDecoder(request.Body).Decode(&input); err != nil {
-			fmt.Println("error 1: ")
+			log.Println("error 1: ")
 			return nil, err
 		}
 		userOutput, err := app.Processor.CreateUser(ctx, &input)
-		fmt.Println("userOutput: ", userOutput)
+		log.Println("userOutput: ", userOutput)
 		if err != nil {
-			fmt.Println("error 2: ")
+			log.Println("error 2: ")
 			return nil, err
 		}
-		fmt.Println("going to be marshalled: ")
+		log.Println("going to be marshalled: ")
 		response, err = json.Marshal(userOutput)
-		fmt.Println("response: ", response)
+		log.Println("response: ", response)
 		if err != nil {
-			fmt.Println("error 3: ")
+			log.Println("error 3: ")
 			return nil, err
 		}
 		return response, nil
