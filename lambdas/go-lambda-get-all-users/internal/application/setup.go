72d632c92d57051a9fceb8a7cb27167d9356da43
Valentin Vila <vilavalentin@gmail.com>
2023-03-13 20:38:10 +0100

    testing env var

--- lambdas/go-lambda-get-all-users/internal/application/setup.go
+++ lambdas/go-lambda-get-all-users/internal/application/setup.go
@@ -16,6 +16,21 @@ func (p *PostgresConnector) GetConnection() (db *gorm.DB, err error) {
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("DB_HOST_WRITER")
 	dbPort := os.Getenv("DB_PORT")
+	if username == "" {
+		fmt.Println("Missing username")
+	}
+	if password == "" {
+		fmt.Println("Missing password")
+	}
+	if dbName == "" {
+		fmt.Println("Missing dbName")
+	}
+	if dbHost == "" {
+		fmt.Println("Missing dbHost")
+	}
+	if dbPort == "" {
+		fmt.Println("Missing dbPort")
+	}
 	if username == "" || password == "" || dbName == "" || dbHost == "" || dbPort == "" {
 		return nil, fmt.Errorf("Missing environment variables for connecting to database")
 	}
