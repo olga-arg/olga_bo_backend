b2a4bea0c976de0d3055122b08230cbf388611b6
Nacho <ignacio.ramos@invera.com.ar>
2023-03-13 23:14:04 -0300

    only update the specified values

--- lambdas/go-lambda-update-card-limit/internal/storage/storage.go
+++ lambdas/go-lambda-update-card-limit/internal/storage/storage.go
@@ -25,32 +25,32 @@ func getUserTable(userID string) func(tx *gorm.DB) *gorm.DB {
 }
 
 func (r *UserRepository) UpdateUserCardLimit(userID string, purchaseLimit int, monthlyLimit int) (*domain.User, error) {
-	user := r.db.Scopes(getUserTable(userID)).Where("id = ?", userID)
+	// Get the current user
+	user := &domain.User{}
+	err := r.db.Scopes(getUserTable(userID)).Where("id = ?", userID).First(user).Error
+	if err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, errors.Wrap(err, "user not found")
+		}
+		log.Println("Error getting user by ID:", err)
+		return nil, errors.Wrap(err, "failed to get user by ID")
+	}
 
-	data := make(map[string]interface{})
+	// Update only the specified field
 	if purchaseLimit != -1 {
-		data["purchase_limit"] = purchaseLimit
+		user.PurchaseLimit = purchaseLimit
 	}
 	if monthlyLimit != -1 {
-		data["monthly_limit"] = monthlyLimit
+		user.MonthlyLimit = monthlyLimit
 	}
 
-	var updatedUser domain.User
-	result := user.Updates(data)
+	// Save the updated user
+	result := r.db.Save(user)
 	if result.Error != nil {
 		log.Println("Error updating user card limit:", result.Error)
 		return nil, errors.Wrap(result.Error, "failed to update user card limit")
 	}
-	if result.RowsAffected == 0 {
-		log.Println("No user card found for update")
-		return nil, errors.New("no user card found for update")
-	}
-	err := user.First(&updatedUser).Error
-	if err != nil {
-		log.Println("Error getting user after update:", err)
-		return nil, errors.Wrap(err, "failed to get user after update")
-	}
-	return &updatedUser, nil
+	return user, nil
 }
 
 func (r *UserRepository) GetUserByID(userID string) (*domain.User, error) {
