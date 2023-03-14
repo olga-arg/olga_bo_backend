f3c2bb0aed1f0f4864e72fe58f5945ac00cdbbd5
Valentin Vila <vilavalentin@gmail.com>
2023-03-15 00:58:47 +0100

    min change

--- lambdas/go-lambda-create-user/pkg/handler/handler.go
+++ lambdas/go-lambda-create-user/pkg/handler/handler.go
@@ -39,7 +39,7 @@ func (h *CreateUserHandler) Handle(request events.APIGatewayProxyRequest) (event
 			Body:       err.Error(),
 		}, nil
 	}
-
+	// Send email to user
 	body := "Hola " + input.Name + ",\n\n¡Bienvenido a Olga!\nAhora tienes acceso a la mejor forma de administrar los gastos en tu empresa.\n\nPara empezar, descarga nuestra app móvil en tu dispositivo:\n\n[enlace para descargar la app iOS/Android]\n\nUna vez que hayas descargado la aplicación, haz click en el siguiente enlace para comenzar tu procesa de registro y comenzar a disfrutar de los siguientes beneficios:\nAprobación de gastos de manera instantanea\nTarjetas Fisicas o Virtuales para ti y tus compañeros de trabajo\nNada de guardar el ticket para presentarlo en contabilidad, ahora solo basta una foto en el momento de la compra y listo!\nNo dudes en ponerte en contacto con nuestro equipo de soporte si tienes alguna pregunta o necesitas ayuda para empezar.\n\nSaludos cordiales,\nEl equipo de Olga"
 	fmt.Println("Sending email from handler to: " + input.Email)
 	services.NewDefaultEmailService().SendEmail("Bienvenido a Olga :)", body, input.Email, nil)
