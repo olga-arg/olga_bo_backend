252507f5be3073ffc88c64ac0eb8d63b1a2304db
Valentin Vila <vilavalentin@gmail.com>
2023-03-14 15:16:58 +0100

    remove a log.Println from email Service

--- lambdas/go-lambda-create-user/internal/services/email_service.go
+++ lambdas/go-lambda-create-user/internal/services/email_service.go
@@ -40,7 +40,6 @@ type EmailSender interface {
 }
 
 func newEmailService(config Config) EmailSender {
-	log.Println("Creating email service with from email: ", config.fromEmailAddress, " and password: ", config.fromEmailPassword[0])
 	auth := smtp.PlainAuth("", config.fromEmailAddress, config.fromEmailPassword, smtpAuthAddress)
 	return &emailService{fromEmail: config.fromEmailAddress, auth: auth}
 }
